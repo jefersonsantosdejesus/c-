5005e193527637632185c41719ea97ff7d553d92
jefersonsantosdejesus <127164641+jefersonsantosdejesus@users.noreply.github.com>
2025-09-22 19:58:13 -0300

    Update aula.cpp

--- aula.cpp
+++ aula.cpp
@@ -33,17 +33,33 @@ int main(){
     printf("NUMERO DOS PONTOS TURISTICOS:%d\n", ponto);
 
 
-    /*%d: Imprime um inteiro no formato decimal.
-     
-    %i: Equivalente a %d.
-     
-    %f: Imprime um número de ponto flutuante no formato padrão.
-     
-    %e: Imprime um número de ponto flutuante na notação científica.
-     
-    %c: Imprime um único caractere.
-     
-    %s: Imprime uma cadeia (string) de caracteres.*/
+
+
+    printf ("digite o estado ");
+    scanf ("%s", estado);
+    printf("digite o codigo");
+    scanf("%s", codigo);
+    printf("digite a cidade");
+    scanf("%s", cidade);
+    printf("digite o população");
+    scanf("%d", &populacao);
+    printf("digite a area");
+    scanf("%f", &area);
+    printf("digite o pib da sua cidade");
+    scanf("%f", &pib);
+    printf("digite o pontos turisticos em numeros");
+    scanf("%d", &ponto);
+
+    printf("Carta 2:");
+    printf("ESTADO:%s\n", estado);
+    printf("CODIGO:%s\n", codigo);
+    printf("NOME DA CIDADE:%s\n", cidade ,"KM");
+    printf("POPULAÇÃO:%d\n",  populacao);
+    printf("ÁREA:%.2f\n", area);  \
+    printf("PIB:.%.2f\n", pib);
+    printf("NUMERO DOS PONTOS TURISTICOS:%d\n", ponto);
+
+    
 
 
 
